2f5cfcfd84fa56bb9942e451ddedfb6279533126
Sergi Romeu <romeuhd@gmail.com>
2019-05-19 20:50:12 +0200

    feat: prepare app.js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,49 @@
+const readline = require("readline");
 const {
-  inputDrone,
+  startingPosition,
+  commands,
   inputLimits,
-  printDrone,
-  processCommands
+  printDrone
 } = require("./utils");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  prompt: "CONSOLE> "
+});
+
+let mode = 0;
+let limits;
+let initialPosition;
+let actualDrone;
+const drones = [];
+
+console.log("Enter the map limits");
+rl.prompt();
+
+rl.on("line", line => {
+  switch (mode) {
+    case 0:
+      limits = inputLimits(line);
+      mode++;
+      console.log("Enter the drone starting position and orientation");
+      break;
+    case 1:
+      initialPosition = startingPosition(limits, line);
+      mode++;
+      console.log("Enter the commands to be executed by the drone");
+      break;
+    case 2:
+      actualDrone = commands(initialPosition, limits, line);
+      drones.push(actualDrone);
+      mode--;
+      console.log("Enter the drone starting position and orientation");
+      break;
+    default:
+      break;
+  }
+  rl.prompt();
+}).on("close", () => {
+  printDrone(drones);
+  process.exit(0);
+});
